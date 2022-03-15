f0893379f89ec86f1b960c2fd5166d9f3e8cd95f
9rotama <aoim99.sc@gmail.com>
2022-03-15 21:22:04 +0900

    HPの作成

---
+++ .cache/sync-requires.js
@@ -0,0 +1,11 @@
+
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": preferDefault(require("/Users/m9/tegeiweb/tegei.github.io/.cache/dev-404-page.js")),
+  "component---src-pages-404-js": preferDefault(require("/Users/m9/tegeiweb/tegei.github.io/src/pages/404.js")),
+  "component---src-pages-index-js": preferDefault(require("/Users/m9/tegeiweb/tegei.github.io/src/pages/index.js"))
+}
+
