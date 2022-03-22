1c2f43cd9f11cc535f6926cd6c7344cabea17ed9
9rotama <aoim99.sc@gmail.com>
2022-03-22 14:05:54 +0900

    不要なファイル削除

--- .cache/page-ssr/routes/render-page.js
+++ .cache/page-ssr/routes/render-page.js
@@ -23,8 +23,7 @@ module.exports = Object.assign;
 
 exports.components = {
   "component---cache-dev-404-page-js": () => __webpack_require__.e(/*! import() | component---cache-dev-404-page-js */ "component---cache-dev-404-page-js").then(__webpack_require__.bind(__webpack_require__, /*! ./../../dev-404-page.js */ "./.cache/dev-404-page.js")),
-  "component---src-pages-404-js": () => __webpack_require__.e(/*! import() | component---src-pages-404-js */ "component---src-pages-404-js").then(__webpack_require__.bind(__webpack_require__, /*! ./../../../src/pages/404.js */ "./src/pages/404.js")),
-  "component---src-pages-index-js": () => __webpack_require__.e(/*! import() | component---src-pages-index-js */ "component---src-pages-index-js").then(__webpack_require__.bind(__webpack_require__, /*! ./../../../src/pages/index.js */ "./src/pages/index.js"))
+  "component---src-pages-404-js": () => __webpack_require__.e(/*! import() | component---src-pages-404-js */ "component---src-pages-404-js").then(__webpack_require__.bind(__webpack_require__, /*! ./../../../src/pages/404.js */ "./src/pages/404.js"))
 };
 
 /***/ }),
--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -5,7 +5,6 @@ const preferDefault = m => (m && m.default) || m
 
 exports.components = {
   "component---cache-dev-404-page-js": preferDefault(require("/Users/m9/tegeiweb/tegei.github.io/.cache/dev-404-page.js")),
-  "component---src-pages-404-js": preferDefault(require("/Users/m9/tegeiweb/tegei.github.io/src/pages/404.js")),
-  "component---src-pages-index-js": preferDefault(require("/Users/m9/tegeiweb/tegei.github.io/src/pages/index.js"))
+  "component---src-pages-404-js": preferDefault(require("/Users/m9/tegeiweb/tegei.github.io/src/pages/404.js"))
 }
 
