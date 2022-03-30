b51d2228fac3c9338ed7291c05d0d8cf0ba6407b
9rotama <aoim99.sc@gmail.com>
2022-03-30 21:02:54 +0900

    worksページ作成

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -5,6 +5,7 @@ const preferDefault = m => (m && m.default) || m
 
 exports.components = {
   "component---cache-dev-404-page-js": preferDefault(require("/Users/m9/tegeiweb/tegei.github.io/.cache/dev-404-page.js")),
-  "component---src-pages-404-js": preferDefault(require("/Users/m9/tegeiweb/tegei.github.io/src/pages/404.js"))
+  "component---src-pages-404-js": preferDefault(require("/Users/m9/tegeiweb/tegei.github.io/src/pages/404.js")),
+  "component---src-pages-works-index-js": preferDefault(require("/Users/m9/tegeiweb/tegei.github.io/src/pages/works/index.js"))
 }
 
