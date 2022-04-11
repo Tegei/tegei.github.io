8e719e534570075fcf4bfdf48cd1c7e73c2ebd86
suzakutakumi <suzakutakumi3@gmail.com>
2022-04-11 12:55:22 +0900

    修正完了

--- src/components/getMember.js
+++ src/components/getMember.js
@@ -1,23 +1,39 @@
+import * as React from "react";
 export default{}
 
 const memberListURL="https://raw.githubusercontent.com/Tegei/tegei.github.io/main/memberList.csv"
 
-export const getMembers= async (dataLength=5)=>{
+export const getMembers= async (dataLength=6)=>{
     const res = await fetch(memberListURL)
     const membersCsv = await res.text()
-    const membersObj=[]
-    for(const memberText of membersCsv.split('\n')){
-        const text=memberText.split(',')
-        if(text.length===dataLength){
-            continue
+    const datas=membersCsv.split(',')
+    return [...Array(Math.floor(datas.length/dataLength))].map((v,k)=>{
+        const link=<>{datas[dataLength*k+3].split('\n').map((row,i)=>{
+            const pos=row.indexOf(':')
+            return(
+                <>
+                {i!==0&&<br/>}
+                <a target="_blank" rel="noopener noreferrer" href={row.slice(pos+1)}>{row.slice(0,pos)}</a>
+                </>
+            )
+        })}</>
+        const free=<>{datas[dataLength*k+4].split('\n').map((row,i)=>{
+            return(
+                <>
+                {i!==0&&<br/>}
+                {row}
+                </>
+            )
+        })}</>
+        const obj={
+            name:datas[dataLength*k],
+            grade:datas[dataLength*k+1],
+            hobby:datas[dataLength*k+2],
+            link,
+            free,
+            imagePath:datas[dataLength*k+5],
         }
-        membersObj.push({
-            name:text[0],
-            grade:text[1],
-            tech:text[2],
-            free:text[3],
-            imagePath:text[4],
-        })
-    }
-    return membersObj
+        console.log(obj)
+        return obj
+    })
 }
